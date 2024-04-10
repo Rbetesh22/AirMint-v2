2bd348bab6b8761c8210ff35b6df890e03a6004e
Ralph Betesh <95585754+Rbetesh22@users.noreply.github.com>
2024-04-09 22:26:27 -0400

    Update AirmintProvider.js

--- npmpackage/lib/components/AirmintProvider.js
+++ npmpackage/lib/components/AirmintProvider.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AirmintProvider = ({children}) => {
 
     return (
-        <div>{children}</div> //wrap it with whatever providers you need
+        <div>{children}</div> 
     )
 }
 
